9b97ab3ac66220b3260b0ecfc6f175330422332a
Rio Adjie Wiguna <rioadjie100301@gmail.com>
2023-01-03 15:12:08 +0700

    visi misi

--- resources/views/HalamanUtama/TentangDesa/visimisi.blade.php
+++ resources/views/HalamanUtama/TentangDesa/visimisi.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="div">
-        @forelse ($visimisi as $item)
         <div class="text-center text-black-60" style="margin-top: 100px">
             <h2>Visi Misi Desa Tambong</h2>
             <hr style="height:10px;background:#d3ea3e;border:4px double #ffffff;">
         </div>
+        @forelse ($visimisi as $item)
         <div class="card mb-3">
             <div class="card-body">
               <h3 class="card-title text-center">Visi</h3><hr>
