eb52ab29494a443b7da1e04823ddde0bf25959e0
Rio Adjie Wiguna <rioadjie100301@gmail.com>
2023-01-05 00:30:15 +0700

    Sidebar

--- resources/views/AdminDashboard/Template2/sidebar.blade.php
+++ resources/views/AdminDashboard/Template2/sidebar.blade.php
@@ -129,8 +129,8 @@
                         <a class="collapse-item" href="{{ route('slide.index') }}"><i class="fas fa-fw fa-sliders"></i> Slide Banner</a>
                         <a class="collapse-item" href="{{ route('aparatur.index') }}"><i class="fas fa-fw fa-people-group"></i> Aparatur Desa</a>
                         <a class="collapse-item" href="{{ route('visi-misi.index') }}"><i class="fas fa-fw fa-bookmark"></i> Visi Misi</a>
-                        <a class="collapse-item" href="#"><i class="fas fa-fw fa-info"></i> Informasi Desa</a>
                         <a class="collapse-item" href="{{ route('galeri.index') }}"><i class="fas fa-fw fa-folder-open"></i> Galeri Desa</a>
+                        <a class="collapse-item" href="#"><i class="fas fa-fw fa-info"></i> Informasi Desa</a>
                     </div>
                 </div>
             </li>
