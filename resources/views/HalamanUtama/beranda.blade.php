b746e52dadef77610b272df589f7affc2629f51a
Rio Adjie Wiguna <rioadjie100301@gmail.com>
2023-01-07 11:00:06 +0700

    Add Produk in Beranda

--- resources/views/HalamanUtama/beranda.blade.php
+++ resources/views/HalamanUtama/beranda.blade.php
@@ -77,28 +77,21 @@
         <hr style="height:5px;background:#d3ea3e;border:2px double #ffffff; margin-top:-5px;">
         </div>
         @forelse ($produk as $row)
-        <div class="col-md-4 mt-2">
+        <div class="col-md-4 mt-2 mb-2">
             <div class="card">
-                <a href="#">
+                <a href="{{ route('detail-produk', $row->slug) }}">
                     <img src=" {{ asset('uploads/' . $row->foto ) }} " class="card-img-top" height="250" alt="...">
                 </a>
                 <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="#" style="text-decoration: none; color: black;">{{ $row ->nama_produk }}</a>
-                    </h5>
-                    <p class="card-text">{!! Str::limit($row->deskripsi, 100) !!}</p>
-                </div>
-                <div class="card-footer" style="background-color: rgb(255, 255, 255);">
-                    <a href="#" class="badge bg-warning" style="text-decoration: none; margin-bottom: 5px;"><i class="bi bi-tag"></i> {{ $row->kategori_produk->nama_kategori }}</a>
-                    <span class="badge bg-success"><i class="bi bi-person-lines-fill"></i>
-                        {{ $row->toko->nama_toko}}
-                    </span>
+                    <a href="{{ route('produk-kategori', $row->kategori_produk->slug) }}" class="badge bg-warning" style="text-decoration: none; margin-bottom: 5px;"><i class="bi bi-tag"></i> {{ $row->kategori_produk->nama_kategori }}</a>
+                    <a href="{{ route('toko-desa', $row->toko->slug) }}" class="badge bg-success" style="text-decoration: none; margin-bottom: 5px;"><i class="bi bi-shop"></i> {{ $row->toko->nama_toko}}</a>
                     <span class="badge bg-primary"><i class="bi bi-eye-fill"></i></i>
                         {{ $row->views }}
                    </span>
-                    <span class="badge bg-secondary"><i class="bi bi-calendar-check"></i>
-                         {{ $row->created_at}}
-                    </span>
+                    <p class="card-title mt-2">
+                        <a href="{{ route('detail-produk', $row->slug) }}" style="text-decoration: none; color: black;">{{ $row ->nama_produk }}</a>
+                    </p>
+                    <h3 class="card-text">{{ $row->harga }}</h3>
                 </div>
             </div>
         </div>
